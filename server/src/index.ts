d6f5ecbac040393095e37ae4184c82811774ddfb
goran <goran.novacic@gmail.com>
2020-12-07 21:05:19 +0100

    #incapsulated_refreshToken

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { createAccessToken, createRefreshToken } from './auth';
 import 'dotenv/config';
 import { createConnection } from 'typeorm';
 import "reflect-metadata"
@@ -5,14 +6,45 @@ import express from 'express'
 import {ApolloServer} from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
 import { UserResolver } from './UserResolver';
+import cookieParser from 'cookie-parser';
+import { verify } from 'jsonwebtoken'
+import { User } from "./entity/User";
+import { sendRefreshToken } from './sendRefreshToken';
+
 
 
 (async () => {
-    const app = express()
+    const app = express();
+    app.use(cookieParser());
     app.get('/', (_req, res) => res.send('hello'));
+    app.post('/refresh_token', async (req, res) => {
+      const token = req.cookies.jid
+        if(!token) {
+          return res.send({ ok: false, accessToken:''})
+        }
 
-    app.post('/refresh_token', req => {
-      console.log(req.headers)
+          
+        let payload: any = null
+        try {
+          payload = verify(token,process.env.REFRESH_TOKEN_SECRET!)
+        } catch (err) {
+          console.log(err);
+          return res.send({ ok: false, accessToken:''})
+        }
+
+        // token is valid and we can 
+        // set back an access token
+        const user = await User.findOne({ id: payload.userId })
+        
+        if(!user) {
+          return res.send({ ok: false, accessToken:''})
+          
+        }
+      
+        sendRefreshToken(res, createRefreshToken(user) )
+  
+          return res.send({ ok: true, accessToken: createAccessToken(user) });
+
     });
 
     // insomnia
