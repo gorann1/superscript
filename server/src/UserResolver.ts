7167b0b4675f1f06a6cb345caaa172311e8c7644
goran <goran.novacic@gmail.com>
2020-12-07 18:47:06 +0100

    first_commit_set_auth_cookies_graphql

---
+++ server/src/UserResolver.ts
@@ -0,0 +1,106 @@
+import { createRefreshToken, createAccessToken } from './auth';
+import { MyContext } from './MyContext';
+import { 
+  Ctx, 
+  Field, 
+  ObjectType, 
+  UseMiddleware 
+} from 'type-graphql';
+import { 
+  Arg, 
+  Mutation, 
+  Query, 
+  Resolver 
+} from 'type-graphql';
+import { hash, compare } from 'bcryptjs';
+import { User } from './entity/User';
+import { isAuth } from './isAuth';
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
+
+@Resolver()
+
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return 'Hi';
+  }
+
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(
+    @Ctx() {payload}: MyContext) {
+      console.log(payload);
+    return `your user ID is: ${payload!.userId}`;
+  }
+
+  
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  
+  @Mutation(() => LoginResponse)
+  
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+    @Ctx() {res}: MyContext
+    ): Promise < LoginResponse>
+    {
+
+    const user = await User.findOne({ where: {email} });
+
+    if(!user) {
+      throw new Error ('could not find user')
+    }
+
+    const valid =await compare(password, user.password)
+
+    if(!valid) {
+      throw new Error ('bad password')
+    }
+
+    // login succesfull
+    res.cookie('jid', createRefreshToken(user), {
+        httpOnly: true
+      }
+    );
+
+
+    return {
+      accessToken: createAccessToken(user)
+     };
+  }
+
+
+  @Mutation(() => Boolean)
+  
+  async register(
+    @Arg('email') email: string,
+    @Arg('password') password: string,
+  ) {
+
+    const hashedPassword = await hash(password, 12) 
+
+    try { await User.insert({
+      email,
+      password: hashedPassword
+    });
+  } catch (err){
+      console.log(err);
+      return false;
+  }
+
+    return true
+  }
+
+
+}
---
+++ server/src/index.ts
@@ -0,0 +1,56 @@
+import 'dotenv/config';
+import { createConnection } from 'typeorm';
+import "reflect-metadata"
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { UserResolver } from './UserResolver';
+
+
+(async () => {
+    const app = express()
+    app.get('/', (_req, res) => res.send('hello'));
+
+    app.post('/refresh_token', req => {
+      console.log(req.headers)
+    });
+
+    // insomnia
+
+     
+    await createConnection()
+    
+
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [ UserResolver ]
+      }),
+      
+      context:({ req, res }) => ({ req, res  })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+
+    app.listen(4000, () => {
+        console.log('express server started'); 
+    }) 
+})()
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
